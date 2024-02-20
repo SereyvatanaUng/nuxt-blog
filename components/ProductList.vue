a1cbf2e7321700928e8c9b9193aee5971c6122e8
SereyvatanaUng <sereyvatanau@gmail.com>
2024-02-20 13:52:25 +0700

    Data Fetching and Displaying

---
+++ components/ProductList.vue
@@ -0,0 +1,32 @@
+<template>
+  <p class="mb-10">Take a look at my Github projects!</p>
+
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">Something went wrong...</section>
+  <section v-else>
+    <ul class="grid grid-cols-1 gap-4">
+      <li
+        v-for="repository in repos"
+        :key="repository.id"
+        class="border border-gray-200 round-sm p-4 hover:bg-gray-100 font-mono"
+      >
+        <a :href="repository.html_url" target="_blank">
+          <div class="flex items-center justify-between">
+            <div class="font-semibold">{{ repository.name }}</div>
+            <div>{{ repository.stargazers_count }}&#9733;</div>
+          </div>
+          <p class="text-sm">{{ repository.description }}</p>
+        </a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+const { error, pending, data } = await useFetch(
+  "https://api.github.com/users/SereyvatanaUng/repos"
+);
+
+// filter out the repo that does not have the description
+const repos = computed(() => data.value.filter((repo) => repo.description));
+</script>
