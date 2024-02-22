22f338228db0b222f57885f1de6c92dd1d114f6b
SereyvatanaUng <sereyvatanau@gmail.com>
2024-02-22 09:18:06 +0700

    Query Content

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>All post!</div>
+  <section>
+    <ul>
+      <li v-for="post in posts" :key="post._path">
+        <NuxtLink to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script setup>
+// useAsyncData to prevent duplicate fetching data
+const { data: posts } = await useAsyncData(() =>
+  queryContent("/blog").only(["_path", "title"]).find()
+);
+console.log(posts);
+</script>
