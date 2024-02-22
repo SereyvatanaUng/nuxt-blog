3a0cc32b2c55ba1b87bc276efb2dfdb233b887aa
SereyvatanaUng <sereyvatanau@gmail.com>
2024-02-22 16:37:23 +0700

    Table of content

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,5 @@
 <template>
-  <article>
-    <ContentDoc
-      path="/about"
-      class="prose dark:prose-invert prose-h1:text-2xlKO"
-    />
+  <article class="prose dark:prose-invert max-w-none">
+    <ContentDoc path="/about" />
   </article>
 </template>
